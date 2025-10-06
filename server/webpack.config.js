8f7951090dca92d1e10f149811cf03c353d13d23
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-10-06 19:51:08 +0200

    remove unused copyright header

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,8 +1,5 @@
 'use strict';
-/*
- * Part of Silesian University of Technology project.
- * Created only for learning purposes.
- */
+
 const dotenv = require('dotenv');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
